9d666618f9b20cd28817716d29ff8880207a2779
limoiie <limo.iie4@gmail.com>
2023-04-07 11:15:51 +0800

    refactor: Add log console component for showing progress logs

---
+++ components/LogConsole.vue
@@ -0,0 +1,30 @@
+<template>
+  <a-list :data-source="logs" ref="vLogs" bordered>
+    <!--suppress VueUnrecognizedSlot -->
+    <template #renderItem="{ item }">
+      <a-list-item>
+        <!--suppress TypeScriptUnresolvedReference -->
+        <pre>{{ item.level }}: {{ item.message }}</pre>
+      </a-list-item>
+    </template>
+
+    <!--suppress VueUnrecognizedSlot -->
+    <template #footer class="h-20" />
+  </a-list>
+</template>
+
+<script setup lang="ts">
+const { logs } = defineProps<{ logs: LogMessage[] }>();
+</script>
+
+<style lang="sass">
+.ant-list-items
+  padding: 8px 0 8px
+
+li
+  &.ant-list-item
+    padding: 0
+
+  pre
+    margin-bottom: 0
+</style>
--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,4 @@
-export { };
+import { Ref } from "vue";
 
 declare global {
   interface CreateEmbeddingsConfigFormState {
@@ -13,4 +13,51 @@ declare global {
     meta: any;
   }
 
+  interface OpenAIEmbeddingsConfigMeta {
+    apiKey: string,
+  }
+
+  interface PineconeVectorstoreConfigMeta {
+    apiKey: string,
+    indexName: string,
+    environment: string,
+    namespace: string,
+    dimension: string,
+    metric: string,
+  }
+
+  type LogLevel = 'info' | 'error' | 'warning';
+
+  interface LogMessage {
+    level: LogLevel;
+    message: string;
+  }
+}
+
+export class Logger {
+  logs: Ref<LogMessage[]>;
+
+  constructor(logs: Ref<LogMessage[]>) {
+    this.logs = logs || [];
+  }
+
+  log(level: LogLevel, message: string) {
+    this.logs.value.push({ level, message });
+  }
+
+  info(message: string) {
+    this.log('info', message);
+  }
+
+  error(message: string) {
+    this.log('error', message);
+  }
+
+  warning(message: string) {
+    this.log('warning', message);
+  }
+
+  reset() {
+    this.logs.value = [];
+  }
 }
